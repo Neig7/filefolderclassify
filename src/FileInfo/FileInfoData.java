15a559347360a2bd52d063feb22be17a15bab4fe
Neig7 <7738rudh@gmail.com>
2017-06-22 00:15:17 +0900

    first commit

---
+++ src/FileInfo/FileInfoData.java
@@ -0,0 +1,9 @@
+package FileInfo;
+
+/**
+ * Created by Neig on 2017/06/19.
+ */
+public class FileInfoData {
+
+
+}
---
+++ src/sample/Controller.java
@@ -0,0 +1,20 @@
+package sample;
+
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+import java.awt.event.ActionEvent;
+import java.io.File;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+
+
+//public class Controller implements Initializable{
+public class Controller{
+
+
+}
